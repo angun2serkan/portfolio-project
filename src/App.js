3d4a439781d996ac451ae29eab46950a5aaf6d7e
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-21 11:39:55 +0300

    Started Intro but could not find nice icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import classes from './App.module.css';
+import Intro from './components/intro/Intro';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className={classes.App}>
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import classes from './Intro.module.css';
 
 function Intro() {
   return (
-    <div>Intro</div>
+    <div className={classes.intro}>
+        <div className={classes.intro__left}>
+            <div className={classes.intro__name}>
+                <span>Hey! I am </span>
+                <span>Serkan Angün </span>
+                <span>Frontend Developer with low level experinece and sky rocket level of patience in web designing and development.</span>
+            </div>
+
+            <button className={`${classes.intro__button} ${classes.button}`}>
+                <span>Lets Meet!</span>
+                <br/>
+                <span>&</span>
+                <br/>
+                <span>Talk Business</span>
+            </button>
+            
+            <div className={classes.intro__icons}>
+              <img src="https://github.com/angun2serkan" alt="github" />
+              <img src="https://www.linkedin.com/in/serkan-ang%C3%BCn/" alt="linkedin" />
+            </div>
+
+        </div>
+        <div className={classes.intro__right}> Right Side</div>
+    </div>
   )
 }
 
